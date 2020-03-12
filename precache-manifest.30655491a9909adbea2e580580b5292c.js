3c9beb980593eb7405d12a919e9c4f5a4f0a8fce
Emilio <emilio.costa@dextra-sw.com>
2020-03-12 15:29:03 -0300

    Updates

---
+++ precache-manifest.30655491a9909adbea2e580580b5292c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287bb848b376d0ccb2bfd73802dd69a4",
+    "url": "/daily-nasa/index.html"
+  },
+  {
+    "revision": "bbf91090ac37d12ca1ef",
+    "url": "/daily-nasa/static/css/2.c07aa688.chunk.css"
+  },
+  {
+    "revision": "cedbb7c8229968c6b8d4",
+    "url": "/daily-nasa/static/css/main.56c9ee74.chunk.css"
+  },
+  {
+    "revision": "bbf91090ac37d12ca1ef",
+    "url": "/daily-nasa/static/js/2.455be0d6.chunk.js"
+  },
+  {
+    "revision": "2e95b0bb4fd3dccc58703a2193038327",
+    "url": "/daily-nasa/static/js/2.455be0d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cedbb7c8229968c6b8d4",
+    "url": "/daily-nasa/static/js/main.1a9f296c.chunk.js"
+  },
+  {
+    "revision": "333e77e7d3af45f52684",
+    "url": "/daily-nasa/static/js/runtime-main.6706d100.js"
+  }
+]);
